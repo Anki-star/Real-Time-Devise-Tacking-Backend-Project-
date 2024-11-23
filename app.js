973204cfc7421ac98f518081dba09b8c74ae59d1
Ankit Soni <ankit913134@gmail.com>
2024-11-23 23:53:02 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const app = express();
+const socket = require('socket.io');
+const http = require('http');
+const path = require('path'); 
+
+const server = http.createServer(app);
+const io = socket(server);
+
+app.set('view engine', 'ejs');
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
+io.on('connection', (socket) => {
+    socket.on("send-location", (data) => {
+        io.emit("receive-location",{id:socket.id, ...data});
+    });
+    socket.on("disconnect", () => {
+        io.emit("user-disconnected",socket.id);
+    });
+  console.log('New user connected');
+});
+
+server.listen(2000);
